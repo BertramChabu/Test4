63beb55d1f96a4a897415ef065254aec6a17fa64
IdaAluso <ida.masheti@strathmore.edu>
2024-06-11 08:38:06 +0300

    changed all html to php

--- template/nav.php
+++ template/nav.php
@@ -1,12 +1,12 @@
 <div class="topnav">
-        <a href="index.html">Home</a>
-        <a href="about.html">About Us</a>
-        <a href="people.html">People</a>
-        <a href="registration.html">Registration</a>
-        <a href="contacts.html">Contacts</a>
+        <a href="index.php">Home</a>
+        <a href="about.php">About Us</a>
+        <a href="people.php">People</a>
+        <a href="registration.php">Registration</a>
+        <a href="contacts.php">Contacts</a>
 
     <div class="topnav-right">
-        <a href="signup.html">Sign up</a>
-        <a href="signin.html">Sign in</a>
+        <a href="signup.php">Sign up</a>
+        <a href="signin.php">Sign in</a>
     </div>
 </div>
