50b583ba74b822103862f1d05a6ec38da397b745
IdaAluso <ida.masheti@strathmore.edu>
2024-06-11 08:33:48 +0300

    Created a separate php folder for navigations

---
+++ template/nav.php
@@ -0,0 +1,12 @@
+<div class="topnav">
+        <a href="index.html">Home</a>
+        <a href="about.html">About Us</a>
+        <a href="people.html">People</a>
+        <a href="registration.html">Registration</a>
+        <a href="contacts.html">Contacts</a>
+
+    <div class="topnav-right">
+        <a href="signup.html">Sign up</a>
+        <a href="signin.html">Sign in</a>
+    </div>
+</div>
