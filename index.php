44d0bbdc8660c13e4c5417caee6f8a873f5f0eaf
mellokidnova <145387124+BertramChabu@users.noreply.github.com>
2025-04-08 21:49:04 +0300

    Change the Index.php and Added bootstrap

--- index.php
+++ index.php
@@ -1,119 +1,176 @@
-<?php include_once ("template/heading.php");?>
+<?php include_once("template/heading.php"); ?>
 
-    <body>
-    <?php include_once ("template/nav.php");?>
+<style>
+    body {
+        background-image: url('img/r.jpg');
+        /* Replace with the path to your roses image */
+        background-size: cover;
+        background-attachment: fixed;
+        animation: backgroundScroll 10s linear infinite;
+    }
 
-        <div class="banner">
-            <h1>Header</h1>
+    @keyframes backgroundScroll {
+        0% {
+            background-position: 0 0;
+        }
+
+        100% {
+            background-position: 0 100%;
+        }
+    }
+
+    .container {
+        background-color: rgba(255, 255, 255, 0.17);
+        backdrop-filter: blur(2);
+        /* Add a semi-transparent background for readability */
+        padding: 20px;
+        border-radius: 10px;
+        color: white;
+        font-weight: 700;
+    }
+</style>
+
+<body>
+    <?php include_once("template/nav.php"); ?>
+    <div class="container mt-5">
+        <h2 class="text-center mt-2">The Rose in Bloom: A Symbol of Love, Sacrifice, and Divine Inspiration</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <p>The Rose in full bloom represents love, the source of all good desires and actions, even if at times this means loving
+                    sacrifice, as represented by the thorns. The rose has a supernatural meaning too. Love, with capital letters, is the love
+                    of God. <br>The rose also has a historical meaning associated with the life of Saint Josemaría Escrivá, founder of Opus Dei,
+                    who inspired and encouraged the people who started Strathmore: he once received a divine sign in the form of a carved gilded rose in a very trying moment of his life.</p>
+            </div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <p>The Rose in full bloom represents love, the source of all good desires and actions, even if at times this means loving
+                    sacrifice, as represented by the thorns. The rose has a supernatural meaning too. Love, with capital letters, is the love
+                    of God. <br>The rose also has a historical meaning associated with the life of Saint Josemaría Escrivá, founder of Opus Dei,
+                    who inspired and encouraged the people who started Strathmore: he once received a divine sign in the form of a carved gilded rose in a very trying moment of his life.</p>
+            </div>
         </div>
+        <div class="row">
+            <div class="col-md-4">
+                <p>The Rose in full bloom represents love, the source of all good desires and actions, even if at times this means loving
+                    sacrifice, as represented by the thorns. The rose has a supernatural meaning too. Love, with capital letters, is the love
+                    of God. <br>The rose also has a historical meaning associated with the life of Saint Josemaría Escrivá, founder of Opus Dei,
+                    who inspired and encouraged the people who started Strathmore: he once received a divine sign in the form of a carved gilded rose in a very trying moment of his life.</p>
+            </div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <p>The Rose in full bloom represents love, the source of all good desires and actions, even if at times this means loving
+                    sacrifice, as represented by the thorns. The rose has a supernatural meaning too. Love, with capital letters, is the love
+                    of God. <br>The rose also has a historical meaning associated with the life of Saint Josemaría Escrivá, founder of Opus Dei,
+                    who inspired and encouraged the people who started Strathmore: he once received a divine sign in the form of a carved gilded rose in a very trying moment of his life.</p>
+            </div>
+        </div>
+        <hr>
 
         <div class="row">
-            <div class="content">
-                <h2>Main Content</h2>
-                <h3>Heading 3</h3>
-                <h4>Heading 4</h4>
-                <p>The Rose in full bloom represents love, the source of all good desires and actions, even if at times this means loving 
-                    sacrifice, as represented by the thorns. The rose has a supernatural meaning too. Love, with capital letters, is the love 
-                    of God. <br>The rose also has a historical meaning associated with the life of Saint Josemaría Escrivá, founder of Opus Dei, 
-                    who inspired and encouraged the people who started Strathmore: he once received a divine sign in the form of a carved gilded rose in a very trying moment of his life.</p> 
+            <div class="col-md-12 text-center">
+                <h2 class="text-center">Of Code and Petals: Where Urban Grit Meets Eternal Bloom</h2>
+                <img class="img-responsive rounded-5" src="img/Street.jpg" alt="Street Image" width="40%">
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-7">
+                <h2>My Favourite Programming Languages</h2>
+                <ul class="list-group opacity-75 mt-3">
+                    <li class="list-group-item active" aria-current="true">Java</li>
+                    <li class="list-group-item">SQL</li>
+                    <li class="list-group-item">PHP</li>
+                    <li class="list-group-item">Javascript</li>
+                    <li class="list-group-item">C++</li>
+                    <li class="list-group-item">Python</li>
+                </ul>
+            </div>
+            <div class="col-md-5">
+                <h2 class="text-center">My Favourite Colors</h2>
+                <ol class="list-group mt-3 opacity-75 list-group-numbered">
+                    <li class="list-group-item active">Black</li>
+                    <li class="list-group-item">Blue</li>
+                    <li class="list-group-item">White</li>
+                    <li class="list-group-item">Green</li>
+                    <li class="list-group-item">Grey</li>
+                    <li class="list-group-item">Red</li>
 
-                <p>The Rose in full bloom represents love, the source of all good desires and actions, even if at times this means loving 
-                    sacrifice, as represented by the thorns. The rose has a supernatural meaning too. Love, with capital letters, is the love 
-                    of God. <br>The rose also has a historical meaning associated with the life of Saint Josemaría Escrivá, founder of Opus Dei, 
-                    who inspired and encouraged the people who started Strathmore: he once received a divine sign in the form of a carved gilded rose in a very trying moment of his life.</p>
-
-                    <p>The Rose in full bloom represents love, the source of all good desires and actions, even if at times this means loving 
-                    sacrifice, as represented by the thorns. The rose has a supernatural meaning too. Love, with capital letters, is the love 
-                    of God. <br>The rose also has a historical meaning associated with the life of Saint Josemaría Escrivá, founder of Opus Dei, 
-                    who inspired and encouraged the people who started Strathmore: he once received a divine sign in the form of a carved gilded rose in a very trying moment of his life.</p>
-        <!-- Start of a new paragraph -->
-        <p>I can write my content here on a new paragraph.</p>
-
-        <img src="img/Street.jpg" alt="Street Image" width="40%">
-
-        <ul>
-            <li>Java</li>
-            <li>HTML</li>
-            <li>MySQL</li>
-            <li>PHP</li>
-            <li>JavaScript</li>
-        </ul>
-
-        <ol style="list-style-type: lower-alpha;">
-            <li>Black</li>
-            <li>Green</li>
-            <li>White</li>
-            <li>Purple</li>
-            <li>Orange</li>
-            <li>Yellow</li>
-        </ol>
-
-        <h2>Back to school requirements</h2>
-
-        <table>
-            <tr>
-                <th>sn</th>
-                <th>Name</th>
-                <th>Quantity</th>
-                <th>Unit Price</th>
-            </tr>
-
-            <tr>
-                <td>1.</td>
-                <td>Pen</td>
-                <td>200</td>
-                <td>5 Ksh</td>
-            </tr>
-
-            <tr>
-                <td>2.</td>
-                <td>Book</td>
-                <td>264</td>
-                <td>250 Ksh</td>
-            </tr>
-
-            <tr>
-                <td>3.</td>
-                <td>Ruler</td>
-                <td>58</td>
-                <td>10 Ksh</td>
-            </tr>
-
-            <tr>
-                <td>4.</td>
-                <td>Pen</td>
-                <td>200</td>
-                <td>5 Ksh</td>
-            </tr>
-
-            <tr>
-                <td>5.</td>
-                <td>Book</td>
-                <td>264</td>
-                <td>250 Ksh</td>
-            </tr>
-
-            <tr>
-                <td>6.</td>
-                <td>Ruler</td>
-                <td>58</td>
-                <td>10 Ksh</td>
-            </tr>
-
-            <tr>
-                <th colspan="3">Total</th>
-                <th>530 Ksh</th>
-            </tr>
-        </table>
+                </ol>
+            </div>
         </div>
 
-        <div class="sidebar">
-            <h4>Side Bar</h4>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<br> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </div>
+
+        <hr>
+        <div class="row">
+            <h2 class="text-center">Back to school requirements</h2>
+            <table class="table opacity-75">
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Name</td>
+                        <td>Quantity</td>
+                        <td>Unit Price</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1.</td>
+                        <td>Pen</td>
+                        <td>200</td>
+                        <td>5 Ksh</td>
+                    </tr>
+                    <tr>
+                        <td>2.</td>
+                        <td>Book</td>
+                        <td>264</td>
+                        <td>250 Ksh</td>
+                    </tr>
+                    <tr>
+                        <td>3.</td>
+                        <td>Ruler</td>
+                        <td>58</td>
+                        <td>10 Ksh</td>
+                    </tr>
+                    <tr>
+                        <td>4.</td>
+                        <td>Pen</td>
+                        <td>200</td>
+                        <td>5 Ksh</td>
+                    </tr>
+
+                    <tr>
+                        <td>5.</td>
+                        <td>Book</td>
+                        <td>264</td>
+                        <td>250 Ksh</td>
+                    </tr>
+
+                    <tr>
+                        <td>6.</td>
+                        <td>Ruler</td>
+                        <td>58</td>
+                        <td>10 Ksh</td>
+                    </tr>
+                    <tr>
+                        <th class="table-active" colspan="3">Total</th>
+                        <th>530 Ksh</th>
+                    </tr>
+                </tbody>
+            </table>
         </div>
 
-        <?php include_once ("template/footer.php");?>
-        
-    </body>
+
+    </div>
+
+    <div class="sidebar">
+        <h4>Side Bar</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<br> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+    </div>
+    </div>
+
+
+    <?php include_once("template/footer.php"); ?>
+
+</body>
+
 </html>
--- template/nav.php
+++ template/nav.php
@@ -1,12 +1,42 @@
-<div class="topnav">
-        <a href="index.php">Home</a>
-        <a href="about.php">About Us</a>
-        <a href="people.php">People</a>
-        <a href="registration.php">Registration</a>
-        <a href="contacts.php">Contacts</a>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <!-- Left: Brand -->
+        <a class="navbar-brand" href="#">Ida Aluso</a>
 
-    <div class="topnav-right">
-        <a href="signup.php">Sign up</a>
-        <a href="signin.php">Sign in</a>
+        <!-- Toggler (visible on small screens) -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Right: Nav links -->
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="people.php">People</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="registration.php">Registration</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contacts.php">Contacts</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi bi-person-circle"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item" href="signup.php">Sign In</a></li>
+                        <li><a class="dropdown-item" href="signin.php">Sign up</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
-</div>
+</nav>
